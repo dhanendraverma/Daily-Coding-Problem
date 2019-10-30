6c944a2c756810ce9367e8dfc953d99987ddec75
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2019-10-30 23:30:58 +0530

    Updated solution

--- Day20.cpp
+++ Day20.cpp
@@ -6,3 +6,59 @@ In this example, assume nodes with the same value are the exact same node object
 Do this in O(M + N) time (where M and N are the lengths of the lists) and constant space.
 ******************************************************************************************************************************************/
 
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+	public:
+		int data;
+		Node* next;
+};
+
+int findintersection(Node* head1, Node* head2){
+	set<Node*> visited;
+	while(head1){
+		visited.insert(head1);
+		head1 = head1->next;
+		
+	}
+	
+	while(head2){
+		if(visited.find(head2)!=visited.end())
+			return head2->data;
+		head2 = head2->next;
+	}
+	return -1;
+}
+
+int main() {
+	// your code goes here
+	Node* newNode;
+	
+	Node* head1 = new Node(); 
+    head1->data = 10; 
+  
+    Node* head2 = new Node(); 
+    head2->data = 3; 
+  
+    newNode = new Node(); 
+    newNode->data = 6; 
+    head2->next = newNode; 
+  
+    newNode = new Node(); 
+    newNode->data = 9; 
+    head2->next->next = newNode; 
+  
+    newNode = new Node(); 
+    newNode->data = 15; 
+    head1->next = newNode; 
+    head2->next->next->next = newNode; 
+  
+    newNode = new Node(); 
+    newNode->data = 30; 
+    head1->next->next = newNode; 
+  
+    head1->next->next->next = NULL; 
+    
+    cout<<"intersection point of lists "<<findintersection(head1,head2)<<endl;
+	return 0;
+}
