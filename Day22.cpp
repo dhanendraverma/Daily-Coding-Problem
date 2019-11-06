cf1dbca5bed280e7fc5071f5ffd6275ae9463598
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2019-11-07 00:39:01 +0530

    Update Day22.cpp

--- Day22.cpp
+++ Day22.cpp
@@ -5,3 +5,69 @@ For example, given the set of words 'quick', 'brown', 'the', 'fox', and the stri
 you should return ['the', 'quick', 'brown', 'fox'].
 Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the string "bedbathandbeyond",
 return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond'].
+
+               #include <bits/stdc++.h>
+using namespace std;
+
+string breakWordDP(string word, set<string> dict){
+	int T[word.length()][word.length()];
+	for(int i=0; i < word.length(); i++){
+		for(int j=0; j < word.length() ; j++){
+			T[i][j] = -1; //-1 indicates string between i to j cannot be split
+		}
+	}
+
+//fill up the matrix in bottom up manner
+	for(int l = 1; l <= word.length(); l++){
+    	for(int i=0; i < word.length() -l + 1 ; i++){
+        	int j = i + l-1;
+        	string str = word.substr(i,l);
+        	//if string between i to j is in dictionary T[i][j] is true
+        	if(dict.find(str)!=dict.end()){
+	            T[i][j] = i;
+	            continue;
+        		
+        	}
+        for(int k=i+1; k <= j; k++){
+            if(T[i][k-1] != -1 && T[k][j] != -1){
+                T[i][j] = k;
+                break;
+            }
+        }
+    }
+}
+if(T[0][word.length()-1] == -1){
+    return NULL;
+}
+
+//create space separate word from string is possible
+string buffer = "";
+int i = 0; int j = word.length() -1;
+while(i < j){
+    int k = T[i][j];
+    if(i == k){
+        buffer += word.substr(i, j+1-i);
+        break;
+    }
+    buffer += word.substr(i,k-i) + " ";
+    i = k;
+}
+
+return buffer;
+}
+
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	set<string> dict;
+	string word,input_str;
+	for(int i=0;i<n;i++){
+		cin>>word;
+		dict.insert(word);
+	}
+	cin>>input_str;
+	cout<<breakWordDP(input_str,dict);
+	return 0;
+}
