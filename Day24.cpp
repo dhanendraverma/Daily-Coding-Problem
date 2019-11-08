1304845460e54f80dfdfddbe7a105145c4a485e2
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2019-11-08 12:02:21 +0530

    Added 24th day

---
+++ Day24.cpp
@@ -0,0 +1,63 @@
+/**************************************************************************************************************************************
+This problem was asked by Google.
+Implement locking in a binary tree. A binary tree node can be locked or unlocked only if all of its descendants or ancestors are not 
+locked.Design a binary tree node class with the following methods:
+=>is_locked, which returns whether the node is locked
+=>lock, which attempts to lock the node. If it cannot be locked, then it should return false. Otherwise,
+it should lock it and return true.
+=>unlock, which unlocks the node. If it cannot be unlocked, then it should return false. Otherwise, it should unlock it and return true.
+You may augment the node to add parent pointers or any other property you would like. You may assume the class is used in a 
+single-threaded program, so there is no need for actual locks or mutexes. Each method should run in O(h), where h is the height of 
+the tree.
+***************************************************************************************************************************************/
+
+1.Ime complexity = is_lock - O(1)
+                  lock - O(m+h)
+                  unlock - O(m+h)
+class BinaryTreeLock{
+	struct BinaryTreeNode{
+		bool locked;
+		BinaryTreeNode parent;
+		BinaryTreeNode left;
+		BinaryTreeNode right;
+	}
+	
+	bool isLocked(BinaryTreeNode node){
+		return node.locked;
+	}
+	
+	bool lock(BinaryTreeNode node){
+		if(!checklockCondition(node))
+			return false;
+		node.locked = true;
+		return true;
+	}
+	
+	bool unlock(BinaryTreeNode node){
+		if(!checklockCondition(node))
+			return false;
+		node.locked = false;
+		return true;
+	}
+	
+	bool checklockCondition(BinaryNodeTree node){
+		if(!checklockConditionChild(node.left) || !checklockConditionChild(node.right))
+			return false;
+		BInaryTreeNode parentNode = node.parent;
+		while(parentNode!=NULL){
+			if(parentNode.locked)
+				return false;
+			parentNOde = parentNode.parent
+		}
+	}
+	
+	bool checklockConditionChild(BinaryNodeTree node){
+		if(node==NULL)
+			return true;
+		if(node.locked)
+			return false;
+		if(!checklockConditionChild(node.left) || !checklockConditionChild(node.right))
+			return false;
+	}
+}
+}
