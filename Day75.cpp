37f77482dc9df75341ddc5cbc63762b3c3d0ff78
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2019-11-25 23:38:54 +0530

    Problem 75 added another approach

--- Day75.cpp
+++ Day75.cpp
@@ -12,7 +12,7 @@ it is 0, 2, 6, 9, 11, 15.
 using namespace std;
 
 //DP approach time - O(n^2) 
-int longestIncreasingSubsequence(vector<int> arr){
+int longestIncreasingSubsequenceDP(vector<int> arr){
 	int n= arr.size(),ans=0;
 	vector<int> dp(n);
 	dp[0] = 1;
@@ -26,9 +26,41 @@ int longestIncreasingSubsequence(vector<int> arr){
 	}
 	return ans;
 }
+//a great explanation here : https://www.geeksforgeeks.org/longest-monotonically-increasing-subsequence-size-n-log-n/
+int getCeilIdx(vector<int>& arr,int r,int key){
+	int l = -1,mid;
+	while(r-l>1){
+		mid = l+(r-l)/2;
+		if(arr[mid]>=key)
+			r = mid;
+		else
+			l = mid;
+	}
+	return r;
+}
+
+int longestIncreasingSubsequence(vector<int> arr){
+	int n = arr.size();
+	vector<int> last_ele(n,0);
+	int last_idx = 1;
+	last_ele[0] = arr[0];
+	for(int i=1;i<n;i++){
+		if(arr[i]>last_ele[last_idx-1])
+			last_ele[last_idx++] = arr[i];
+		else if(arr[i]<last_ele[0])
+			last_ele[0] = arr[0];
+		else{
+			last_ele[getCeilIdx(arr,last_idx-1,arr[i])] = arr[i];
+			cout<<getCeilIdx(arr,last_idx-1,arr[i])<<endl;
+		}
+		
+	}
+	return last_idx;
+}
 
 int main() {
-	vector<int> arr = {10, 22, 9, 33, 21, 50, 41, 60};
+	vector<int> arr = 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
 	cout<<longestIncreasingSubsequence(arr)<<endl;
+	cout<<longestIncreasingSubsequenceDP(arr)<<endl;
 	return 0;
 }
