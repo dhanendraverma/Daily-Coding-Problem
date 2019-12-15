a92265414eb82314647dcdb0b225baed07854fb3
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2019-12-15 16:40:30 +0530

    Problem 197

---
+++ Day197.cpp
@@ -0,0 +1,25 @@
+/****************************************************************************************************************************************
+This problem was asked by Amazon.
+Given an array and a number k that's smaller than the length of the array, rotate the array to the right k elements in-place.
+*****************************************************************************************************************************************/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void rotateKright(vector<int>& arr, int k){
+	int n=arr.size()-1;
+	reverse(arr.begin()+k,arr.end());
+	reverse(arr.begin(),arr.begin()+k);
+	reverse(arr.begin(),arr.end());
+}
+
+
+int main() {
+	vector<int> arr = {1,2,3,4,5,6};
+	int k = 4;
+	rotateKright(arr,k);
+	for(auto i:arr)
+		cout<<i<<" ";
+	return 0;
+}
