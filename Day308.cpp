fca578c53dec5baa1caf02f262cfff7b4425ebc3
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2020-06-05 14:22:19 +0530

    Problem 308 Boolean Parenthesization
    added top down  approach

--- Day308.cpp
+++ Day308.cpp
@@ -8,6 +8,66 @@ For example, suppose the input is ['F', '|', 'T', '&', 'T']. In this case, there
 (F | T) & T and F | (T & T).
 *****************************************************************************************************************************/
 
+// ######### TOp down approach #################
+#include <iostream>
+#include <vector>
+using namespace std;
+int solve(string str,int i,int j,vector<vector<int>>& T, vector<vector<int>>& F,bool tf){
+    if(i>j)
+        return 0;
+        
+    if(i==j){
+        T[i][j] = (str[i]=='T'?1:0);
+        F[i][j] = (str[i]=='F'?1:0);
+    }
+    
+    if(tf==true && T[i][j]!=-1)
+        return T[i][j];
+    
+    if(tf==false && F[i][j]!=-1)
+        return F[i][j];    
+     //Recursively solve the problem by breaking the expression at each oprator between i&j and solve left and right part
+     //individually
+    T[i][j] = F[i][j] = 0;
+    for(int k=i+1;k<j;k+=2){
+        T[i][k-1] = solve(str,i,k-1,T,F,true);
+        F[i][k-1] = solve(str,i,k-1,T,F,false);
+        T[k+1][j] = solve(str,k+1,j,T,F,true);
+        F[k+1][j] = solve(str,k+1,j,T,F,false);
+        if(str[k]=='|'){
+            T[i][j] += T[i][k-1]*F[k+1][j]+F[i][k-1]*T[k+1][j]+T[i][k-1]*T[k+1][j];
+            F[i][j] += F[i][k-1]*F[k+1][j];
+        }
+        else if(str[k]=='&'){
+            T[i][j] += T[i][k-1]*T[k+1][j];
+            F[i][j] += T[i][k-1]*F[k+1][j] + F[i][k-1]*T[k+1][j] + F[i][k-1]*F[k+1][j];
+        }
+        else if(str[k]=='^'){
+            T[i][j] += T[i][k-1]*F[k+1][j] + F[i][k-1]*T[k+1][j];
+            F[i][j] += T[i][k-1]*T[k+1][j] + F[i][k-1]*F[k+1][j];
+            
+        }
+    }
+    return tf==true?T[i][j]:F[i][j];
+    
+}
+int main() {
+    string str = "F|T&T";
+    int n = str.length();
+    vector<vector<int>> T(n,vector<int>(n,-1)), F(n,vector<int>(n,-1));
+    cout<<solve(str,0,n-1,T,F,true)<<endl;
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++)
+    //         cout<<T[i][j]<<" ";
+    //     cout<<endl;
+    // }
+    return 0;
+}
+
+
+
+
+//########### Bottom up approach ##############
 #include<iostream> 
 #include <vector>
 #include<cstring> 
