3629b23be87bb25f00e1e3e6c91af7d3492455ef
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2020-06-11 13:18:49 +0530

    Update Day75.cpp

--- Day75.cpp
+++ Day75.cpp
@@ -1,11 +1,11 @@
-/*****************************************************************************************************************************************
+/******************************************************************************************************************************
 Good morning! Here's your coding interview problem for today.
 This problem was asked by Microsoft.
-Given an array of numbers, find the length of the longest increasing subsequence in the array. The subsequence does not necessarily have
-to be contiguous.
-For example, given the array [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15], the longest increasing subsequence has length 6: 
-it is 0, 2, 6, 9, 11, 15.
-*****************************************************************************************************************************************/
+Given an array of numbers, find the length of the longest increasing subsequence in the array. The subsequence does not 
+necessarily have to be contiguous.
+For example, given the array [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15], the longest increasing subsequence has 
+length 6: it is 0, 2, 6, 9, 11, 15.
+******************************************************************************************************************************/
 
 #include <iostream>
 #include <vector>
@@ -26,6 +26,7 @@ int longestIncreasingSubsequenceDP(vector<int> arr){
 	}
 	return ans;
 }
+
 //a great explanation here : https://www.geeksforgeeks.org/longest-monotonically-increasing-subsequence-size-n-log-n/
 int getCeilIdx(vector<int>& arr,int r,int key){
 	int l = -1,mid;
