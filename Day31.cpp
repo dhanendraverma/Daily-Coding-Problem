4438ab1916ad160bb9cffe43c05178ab347aa030
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2020-06-18 23:40:52 +0530

    Problem 31 Minimum edit distance DP
    Added top down approach

--- Day31.cpp
+++ Day31.cpp
@@ -1,10 +1,37 @@
-/****************************************************************************************************************************************
+/******************************************************************************************************************************
 This problem was asked by Google.
-The edit distance between two strings refers to the minimum number of character insertions, deletions, and substitutions required to 
-change one string to the other. For example, the edit distance between “kitten” and “sitting” is three: substitute the “k” for “s”, 
-substitute the “e” for “i”, and append a “g”.
+The edit distance between two strings refers to the minimum number of character insertions, deletions, and substitutions 
+required to change one string to the other. For example, the edit distance between “kitten” and “sitting” is three: substitute
+the “k” for “s”, substitute the “e” for “i”, and append a “g”.
 Given two strings, compute the edit distance between them.
-*****************************************************************************************************************************************/
+******************************************************************************************************************************/
+
+
+// ################################ Top down appraoch #######################################
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int minEditDist(string A,string B,int m,int n,vector<vector<int>>& T){
+    if(m==0 || n==0)
+        return max(m,n);
+    if(T[m][n]!=-1)
+        return T[m][n];
+    if(A[m-1]==B[n-1])
+        return T[m][n] = minEditDist(A,B,m-1,n-1,T);    
+    return T[m][n] = 1+min(minEditDist(A,B,m-1,n,T),min(minEditDist(A,B,m,n-1,T),minEditDist(A,B,m-1,n-1,T)));
+}
+
+int main() {
+    string A = "kitten", B = "sitting";
+    vector<vector<int>> T(A.length()+1,vector<int>(B.length()+1,-1));
+    cout<<minEditDist(A,B,A.length(),B.length(),T)<<endl;
+	return 0;
+}
+
+
+
+// ################################ Bottom up appraoch #######################################
 #include <iostream>
 using namespace std;
 
@@ -17,7 +44,6 @@ int minEditDist(string str1, string str2){
 	for(int i=0;i<=n;i++)
 		dp[i][0] = i;
 	for(int i=1;i<=m;i++){
-		
 		for(int j=1;j<=n;j++){
 			if(str1[i-1]==str2[j-1])
 			{
