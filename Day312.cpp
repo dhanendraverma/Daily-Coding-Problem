2ceda338f1882b8be0aee1232ef0aaf4a2fb6200
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2020-06-25 09:04:34 +0530

    Problem 312 Tiling problem DP
    Added new approach

--- Day312.cpp
+++ Day312.cpp
@@ -1,4 +1,4 @@
-/******************************************************************************************************************************
+/*******************************************************************************************************************************************************************
 This problem was asked by Wayfair.
 You are given a 2 x N board, and instructed to completely cover the board with the following shapes:
 Dominoes, or 2 x 1 rectangles.
@@ -10,11 +10,22 @@ Given an integer N, determine in how many ways this task is possible.
 
 Similar Questions : https://leetcode.com/problems/domino-and-tromino-tiling/
 Explaination : https://www.youtube.com/watch?v=BlT6VCE6lGc
-******************************************************************************************************************************/
+*******************************************************************************************************************************************************************/
 
 #include <iostream>
 #include <vector>
 using namespace std;
+
+int no_of_arrangement(int N){
+    vector<vector<int>> dp(N+1,vector<int>(2)); //of ways to fill board of length i when 1.It's fully filled 2.On corner is left corner
+    T[0][0] = 1;
+    T[1][0] = 1;
+    for (int i = 2; i <= N; ++i) {
+        dp[i][0] = dp[i - 1][0]+ dp[i - 2][0] + dp[i - 1][1] 
+        dp[i][1] = 2 * dp[i - 2][0] + dp[i - 1][1]
+    }
+    return dp[N][0;
+}
 
 int noOfArrangement(int N){
     vector<int> dp(N+1);
