b99f05049d4195d4f7087211773930a395c74729
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2020-09-08 20:18:19 +0530

    Problem 130 Buy & sell stocks dp

--- Day130.cpp
+++ Day130.cpp
@@ -11,15 +11,35 @@ For example, given k = 2 and the array [5, 2, 4, 0, 1], you should return 3.
 using namespace std;
 
 int maxProfit(vector<int> prices, int k){
-	vector<vector<int>> dp(k+1,vector<int>(prices.size()+1,0));
-	for(int i=1;i<=k;i++){
+    int n = prices.size();
+    if(n<=1 || k==0)
+        return 0;
+        
+    //same as infinite transactions
+    if(2*k>=n){
+        int profit = 0;
+        for(int i=0;i<n-1;i++)
+            profit += max(0,prices[i+1]-prices[i]);
+        return profit;
+    }
+    
+     //solve using the dp
+	vector<vector<int>> T(k+1,vector<int>(n));
+	for(int i=0;i<=k;i++){
 		int trackDiff = INT_MIN;
-		for(int j=1;j<=prices.size();j++){
-			trackDiff = max(trackDiff, dp[i-1][j-1]-prices[j-1]);
-			dp[i][j] = max(dp[i][j-1], prices[j]+trackDiff);
+		for(int j=0;j<n;j++){
+		    if(i==0 || j==0)
+		        T[i][j] = 0;
+		    else{
+		        //can retain the max when the buying is the least
+                	//comapre the last buying with the current buying whichever is min
+			trackDiff = max(trackDiff, T[i-1][j-1]-prices[j-1]);
+			//after buying at the least price sell it at the current day i.e. j or don't sell at all
+			T[i][j] = max(T[i][j-1], prices[j]+trackDiff);
+		    }
 		}
 	}
-	return dp[k][prices.size()-1];
+	return T[k][n-1];
 }
 
 int main() {
