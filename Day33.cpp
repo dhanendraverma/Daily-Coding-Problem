8bac4f8eba65e0a28aea7984bd8dba8bebe48d56
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2021-03-01 22:52:02 +0530

    Changed the variable names

--- Day33.cpp
+++ Day33.cpp
@@ -9,52 +9,52 @@ using namespace std;
 
 void running_median(vector<double> arr, int n) 
 { 
-	priority_queue<double> s; 
-	priority_queue<double,vector<double>,greater<double> > g; 
+	priority_queue<double> smaller; 
+	priority_queue<double,vector<double>,greater<double> > greater; 
 	double med = arr[0]; 
 	s.push(arr[0]); 
 	cout << med << endl; 
 	for (int i=1; i < n; i++) 
 	{ 
 		double x = arr[i]; 
-		if (s.size() > g.size()) 
+		if (smaller.size() > greater.size()) 
 		{ 
 			if (x < med) 
 			{ 
-				g.push(s.top()); 
-				s.pop(); 
-				s.push(x); 
+				greater.push(s.top()); 
+				smaller.pop(); 
+				smaller.push(x); 
 			} 
 			else
-				g.push(x); 
+				greater.push(x); 
 
-			med = (s.top() + g.top())/2.0; 
+			med = (smaller.top() + greater.top())/2.0; 
 		} 
-		else if (s.size()==g.size()) 
+		else if (smaller.size()==greater.size()) 
 		{ 
 			if (x < med) 
 			{ 
-				s.push(x); 
-				med = (double)s.top(); 
+				smaller.push(x); 
+				med = (double)smaller.top(); 
 			} 
 			else
 			{ 
-				g.push(x); 
-				med = (double)g.top(); 
+				greater.push(x); 
+				med = (double)greater.top(); 
 			} 
 		} 
 		else
 		{ 
 			if (x > med) 
 			{ 
-				s.push(g.top()); 
-				g.pop(); 
-				g.push(x); 
+				smaller.push(greater.top()); 
+				greater.pop(); 
+				greater.push(x); 
 			} 
 			else
-				s.push(x); 
+				smaller.push(x); 
 
-			med = (s.top() + g.top())/2.0; 
+			med = (smaller.top() + greater.top())/2.0; 
 		} 
 
 		cout << med << endl; 
