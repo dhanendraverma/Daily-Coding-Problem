30e1eca27aeaf10c09b4e3874c375e38d1fe29cf
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2021-03-08 13:32:28 +0530

    Day 26 Delete Kth node from the last

--- Day26.cpp
+++ Day26.cpp
@@ -3,79 +3,72 @@ This problem was asked by Google.
 Given a singly linked list and an integer k, remove the kth last element from the list. k is guaranteed to be smaller than the length 
 of the list.
 The list is very long, so making more than one pass is prohibitively expensive.
-**************************************************************************************************************************************/
-#include <iostream>
+**************************************************************************************************************************************/#include <iostream>
 using namespace std;
 
 class LinkedList{
 	public:
-		class node{
+		class Node{
 			public:
 				int data;
-				node* next;
-				node(int d){
-					data  = d;
-					next = NULL;
+				Node* next;
+				Node(int d){
+					this->data = d;
+					this->next = NULL;
 				}
 		};
-		node* head;
-		node* deleteNode(int nodenum){
-			node *first = head;
-			node *second = head;
-			int i=0;
-			while(i<nodenum){
-				if(second->next==NULL){
-					if(i==nodenum-1)
-						head = head->next;
-					return head;
-				}
-				second = second->next;
-			}
-			
-			while(second->next!=NULL){
-				first = first->next;
-				second = second->next;
-			}
-			
-			first->next = first->next->next;
-			return head;
+		
+		Node* head;
+		
+		LinkedList(){
+			this->head = NULL;
 		}
 		
-		node* push(int new_data){
-			node* new_node = new node(new_data);
-			new_node->next = head;
-			head = new_node;
-			return head;
+		void insert(int d){
+			Node* curr = new Node(d);
+			if(!head)
+				head=curr;
+			else{
+				curr->next = head;
+				head = curr;
+			}
 		}
 		
-		void printList(){ 
-			node* tnode = head; 
-			while (tnode != NULL){ 
-				cout << (tnode->data) << ( " "); 
-				tnode = tnode->next; 
-				
-			} 
-			
-		} 
+		void printList(){
+			Node* temp = head;
+			while(temp){
+				cout<<temp->data<<" ";
+				temp = temp->next;
+			}
+			cout<<endl;
+		}
+		
+		void deleteKthFromLast(int k){
+			Node *fast = this->head, *slow = this->head;
+			while(k--)
+				fast = fast->next;
+			while(fast->next){
+				fast = fast->next;
+				slow = slow->next;
+			}
+			slow->next = slow->next->next;
+		}
 };
 
 
 int main() {
-	LinkedList* llist = new LinkedList(); 
-  
-    llist->head = llist->push(7); 
-    llist->head = llist->push(1); 
-    llist->head = llist->push(3); 
-    llist->head = llist->push(2); 
-  
-    cout<<"Given Linked list is: ";
-    llist->printList(); 
-  
-    int N = 1; 
-    llist->head = llist->deleteNode(N); 
-  
-    cout<<endl<<"Linked List after Deletion is: ";
-    llist->printList();
-	
+	LinkedList ll;
+	ll.insert(1);
+	ll.insert(2);
+	ll.insert(3);
+	ll.insert(4);
+	ll.insert(5);
+	ll.insert(6);
+	ll.insert(7);
+	ll.insert(8);
+	ll.insert(9);
+	ll.printList();
+	ll.deleteKthFromLast(3);
+	ll.printList();
 	return 0;
 }
