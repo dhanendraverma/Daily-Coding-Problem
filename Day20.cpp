2069cd5be154393a9fa5c2938bc78b1edc266164
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2021-03-08 17:43:57 +0530

    Day 20 Intersection point of two in linked list

--- Day20.cpp
+++ Day20.cpp
@@ -6,59 +6,56 @@ In this example, assume nodes with the same value are the exact same node object
 Do this in O(M + N) time (where M and N are the lengths of the lists) and constant space.
 ******************************************************************************************************************************************/
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <unordered_set>
 using namespace std;
+
 class Node{
 	public:
 		int data;
 		Node* next;
+		Node(int data){
+			this->data = data;
+			this->next = NULL;
+		}
 };
 
-int findintersection(Node* head1, Node* head2){
-	set<Node*> visited;
-	while(head1){
-		visited.insert(head1);
-		head1 = head1->next;
+int findIntersectionHash(Node* head1, Node* head2){
+	Node *list1 = head1, *list2 = head2;
+	unordered_set<Node*> visited;
+	
+	while(list1){
+		// cout<<list1->data<<" ";
+		visited.insert(list1);
+		list1 = list1->next;
 		
 	}
-	
-	while(head2){
-		if(visited.find(head2)!=visited.end())
-			return head2->data;
-		head2 = head2->next;
+
+	while(list2){
+		// cout<<list2->data<<" ";
+		if(visited.find(list2)!=visited.end())
+			return list2->data;
+		list2 = list2->next;
 	}
 	return -1;
 }
 
 int main() {
-	// your code goes here
 	Node* newNode;
+	Node* head1 = new Node(10);
+	Node* head2 = new Node(3);
 	
-	Node* head1 = new Node(); 
-    head1->data = 10; 
-  
-    Node* head2 = new Node(); 
-    head2->data = 3; 
-  
-    newNode = new Node(); 
-    newNode->data = 6; 
-    head2->next = newNode; 
-  
-    newNode = new Node(); 
-    newNode->data = 9; 
-    head2->next->next = newNode; 
-  
-    newNode = new Node(); 
-    newNode->data = 15; 
-    head1->next = newNode; 
-    head2->next->next->next = newNode; 
-  
-    newNode = new Node(); 
-    newNode->data = 30; 
-    head1->next->next = newNode; 
-  
-    head1->next->next->next = NULL; 
-    
-    cout<<"intersection point of lists "<<findintersection(head1,head2)<<endl;
+	head2->next = new Node(6);
+    	head2->next->next = new Node(9); 
+
+    	newNode = new Node(15); 
+    	head1->next = newNode; 
+    	head2->next->next->next = newNode; 
+
+    	newNode = new Node(30);
+    	head1->next->next = newNode; 
+    	head2->next->next->next->next = newNode; 
+
+    	cout<<"intersection point of lists uisng hash mehtod: "<<findIntersectionHash(head1,head2)<<endl;
 	return 0;
 }
