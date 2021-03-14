bac3370361fa766848131d92f29cad269fd9285e
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2021-03-14 13:17:40 +0530

    Problem 438 Implement stack using heap

--- Day438.cpp
+++ Day438.cpp
@@ -1,4 +1,4 @@
-/***************************************************************************************************************************************************************
+/**************************************************************************************************************************************************************
 This problem was asked by Amazon.
 Implement a stack API using only a heap. A stack implements the following methods:
     push(item), which adds an element to the stack
@@ -6,7 +6,7 @@ Implement a stack API using only a heap. A stack implements the following method
 Recall that a heap has the following operations:
     push(item), which adds a new key to the heap
     pop(), which removes and returns the max value of the heap
-***************************************************************************************************************************************************************/
+**************************************************************************************************************************************************************/
 
 #include <iostream>
 #include <queue>
