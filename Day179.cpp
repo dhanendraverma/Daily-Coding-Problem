5e0839f26127e2004a4e8ffc94c6870f0da40ede
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2021-04-05 10:29:22 +0530

    Construct BST using postorder

--- Day179.cpp
+++ Day179.cpp
@@ -9,51 +9,53 @@ For example, given the sequence 2, 4, 3, 8, 7, 5, you should construct the follo
 2   4   8
 ***************************************************************************************************************************************/
 
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <iostream>
+#include <climits>
+#include <vector>
+using namespace std;
 
 class Node{
 	public:
-	int data;
-	Node *left, *right;
-	Node(int data) { 
-	this->data = data; 
-	this->left = this->right = NULL; 
+		int val;
+		Node *left, *right;
+		Node(int val){
+			this->val = val;
+			this->left = this->right = NULL;
+		}
+};
+
+Node* constructTreeRec(vector<int>& postorder, int& postIndex, int key, int min, int max){
+	if(postIndex<0)
+		return NULL;
+	Node* root = NULL;
+	if(key > min && key < max){
+		root = new Node(key);
+		postIndex--;
+		if(postIndex >= 0)
+			root->right = constructTreeRec(postorder, postIndex, postorder[postIndex], key, max);
+		if(postIndex >= 0)
+			root->left = constructTreeRec(postorder, postIndex, postorder[postIndex], min, key);
 	}
-}; 
+	return root;
+}
 
-struct Node* BSTrecur(vector<int> postOrder, int& postIndex, int key, int min, int max) { 
-	if(postIndex<0) 
-		return NULL; 
-	Node* root = NULL; 
-	if (key>min && key<max){ 
-		root = new Node(key); 
-		postIndex = postIndex - 1; 
-		if (postIndex>=0){ 
-		root->right = BSTrecur(postOrder, postIndex,postOrder[postIndex],key, max); 
-		root->left = BSTrecur(postOrder, postIndex,postOrder[postIndex],min, key); 
-		} 
-	} 
-	return root; 
-} 
 
-Node* constructBST(vector<int> postOrder) { 
-	int postIndex = postOrder.size()-1; 
-	return BSTrecur(postOrder, postIndex,postOrder[postIndex],INT_MIN, INT_MAX); 
-} 
+Node* constructTree(vector<int> postorder){
+	int postIndex = postorder.size()-1;
+	return constructTreeRec(postorder, postIndex, postorder[postIndex], INT_MIN, INT_MAX);
+}
 
 void printInorder (Node* node){ 
 	if (!node) 
 		return; 
 	printInorder(node->left); 
-	cout<<node->data<<" "; 
+	cout<<node->val<<" "; 
 	printInorder(node->right); 
 } 
 
- 
-int main () { 
-	vector<int> postOrder = {1, 7, 5, 50, 40, 10}; 
-	Node* root = constructBST(postOrder); 
-	printInorder(root); 
-	return 0; 
-} 
+int main() {
+	vector<int> postorder = {1, 7, 5, 50, 40, 10};
+	Node* root = constructTree(postorder);
+	printInorder(root);
+	return 0;
+}
