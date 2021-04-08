4e0824fbeee71689576bb5a5a71ce6bef57dd057
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2021-04-08 17:05:18 +0530

    Problem 1 Find pair with given sum

--- Day1.cpp
+++ Day1.cpp
@@ -1,38 +1,30 @@
-/*---------------Question-----------------------------------------------------------//
-*   Given a list of numbers and a number k,                                         //
-*   return whether any two numbers from the list add up to k.                       //    
-*   For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.  //
-*-----------------------------------------------------------------------------------*/
+/*********************************************************************************************************************************************************
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.                           
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.  
+***********************************************************************************************************************************************************/
 
-#include <bits/stdc++.h>
+#include <unordered_set>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-void find_pair(int arr[], int arr_size, int sum) 
-{ 
+void findPair(vector<int> arr, int k){ 
     unordered_set<int> s; 
-    for(int i = 0; i < arr_size; i++) 
-    { 
-        int temp = sum - arr[i]; 
-  
-        if (temp >= 0 && s.find(temp) != s.end()) 
-            cout << "Pair with given sum " << sum << 
-                 " is (" << arr[i] << ", " << temp << 
-                 ")" << endl; 
-  
+    for(int i = 0; i < arr.size(); i++){ 
+        int temp = k - arr[i]; 
+        if (temp >= 0 && s.find(temp) != s.end()){ 
+        	cout << "Pair with given sum " << k << " is (" << arr[i] << ", " << temp << ")" << endl;
+        	break;
+        }
         s.insert(arr[i]); 
     } 
 } 
 
 
-
 int main()
 {
-    int n,k;
-    cin>>n>>k;
-    int a[n];
-    for(int i=0;i<n;i++)
-        cin>>a[i];
-    find_pair(a,n,k);
-  
+	vector<int> arr = {10, 15, 3, 7};
+	int k = 17;
+	findPair(arr, k);
 }
