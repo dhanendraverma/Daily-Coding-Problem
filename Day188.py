6565a8c440a291a5a3cda7be72692e5910edead7
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2021-05-08 22:35:48 +0530

    Update Day188.py

--- Day188.py
+++ Day188.py
@@ -1,3 +1,4 @@
+'''
 /***************************************************************************************************************************************
 Good morning! Here's your coding interview problem for today.
 This problem was asked by Google.
@@ -17,10 +18,10 @@ for f in functions:
     f()
 How can we make it print out what we apparently want?
 ***************************************************************************************************************************************/
-
+'''
 #Output of the code would be : 3 3 3 reason behind is a closure caputures the variable, not the current variable value, for all the 
-functios being appended to the flist the same variale i is shared among them, in orderd to make this work as expected we can get each 
-function it's own scope variable which would help to maintain the value coming from list in variable named i.
+# functios being appended to the flist the same variale i is shared among them, in orderd to make this work as expected we can get each 
+# function it's own scope variable which would help to maintain the value coming from list in variable named i.
 
 def make_functions():
     flist = []
@@ -34,8 +35,10 @@ def make_functions():
 
 functions = make_functions()
 for f in functions:
-    f()
+    f() 
+#output = 1 2 3
     
-the local binding of the value in variabel i to the scope of the function by using the assignment i==i we can now referece to the value
-of i when the function was being appended to the list.
+    
+# the local binding of the value in variabel i to the scope of the function by using the assignment i==i we can now referece to the value
+# of i when the function was being appended to the list.
     
