db6336498a874ef097a0f9b907122e51592bb231
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2021-05-11 22:58:17 +0530

    Problem 461 No of ways to reach bottom right

---
+++ Day461.cpp
@@ -0,0 +1,45 @@
+/*****************************************************************************************************************************************
+This problem was asked by Facebook.
+There is an N by M matrix of zeroes. Given N and M, write a function to count the number of ways of starting at the top-left corner and 
+getting to the bottom-right corner. You can only move right or down.
+For example, given a 2 by 2 matrix, you should return 2, since there are two ways to get to the bottom-right:
+Right, then down
+Down, then right
+Given a 5 by 5 matrix, there are 70 ways to get to the bottom-right.
+*****************************************************************************************************************************************/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int countWays(int n, int m){
+	vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+	for(int i=0 ;i<=n; i++)
+		dp[i][1] = 1;
+	for(int j=0; j<=m; j++)
+		dp[1][j] = 1;
+	for(int i=2; i<=n; i++){
+		for(int j=2; j<=m; j++)
+			dp[i][j] = dp[i-1][j] + dp[i][j-1];
+	}
+	
+	// for(auto row:dp){
+	// 	for(auto ele:row)
+	// 		cout<<ele<<" ";
+	// 	cout<<endl;
+	// }
+	return dp[n][m];
+}
+
+long long find_ways(int m,int n){
+	if(m==1 || n==1)
+		return 1;
+	return find_ways(m-1,n)+find_ways(m,n-1);
+}
+
+int main() {
+	int n=5,m=5;
+	cout<<find_ways(n,m)<<endl;
+	cout<<countWays(n,m)<<endl;
+	return 0;
+}
