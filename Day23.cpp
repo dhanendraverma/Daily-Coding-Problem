c399af93f966f83d63f7f0b2649385547c135c53
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2021-05-11 11:48:45 +0530

    Problem 23 Min Steps to reach destination

--- Day23.cpp
+++ Day23.cpp
@@ -1,4 +1,4 @@
-/***************************************************************************************************************************************
+/************************************************************************************************************************************************************
 This problem was asked by Google.
 You are given an M by N matrix consisting of booleans that represents a board. Each True boolean represents a wall. 
 Each False boolean represents a tile you can walk on.
@@ -13,52 +13,55 @@ For example, given the following board:
 [f, f, f, f]]
 and start = (3, 0) (bottom left) and end = (0, 0) (top left), the minimum number of steps required to reach the end is 7, 
 since we would need to go through (1, 2) because there is a wall everywhere else on the second row.
-***************************************************************************************************************************************/
+*************************************************************************************************************************************************************/
 
-#include <bits/stdc++.h> 
+#include <iostream>
+#include <queue>
+
 using namespace std; 
-  
-struct queueNode 
-{ 
-    pair<int,int> pt;  
-    int dist; 
-}; 
  
-bool isValid(int row, int col,int ROW, int COL) 
-{ 
+vector<vector<int>> dir = {{-1, 0, 0, 1}, {0, -1, 1, 0}};
+  
+class Node{
+	public:
+		pair<int,int> cord;
+		int dist;
+		Node(pair<int,int> cord, int dist){
+			this->cord = cord;
+			this->dist = dist;
+		}
+};
+ 
+bool isValid(int row, int col,int ROW, int COL){ 
 	return (row >= 0) && (row < ROW) && (col >= 0) && (col < COL); 
 } 
 
-int BFS(vector<vector<char> > mat, pair<int,int> src, pair<int,int> dest) 
-{
-	int rowNum[] = {-1, 0, 0, 1}; 
-	int colNum[] = {0, -1, 1, 0}; 
-	if (mat[src.first][src.second]=='t' || !mat[dest.first][dest.second]=='t') 
-        return -1; 
+int minSteps(vector<vector<char>> matrix, pair<int,int> src, pair<int,int> dest){
 	
-	bool visited[mat.size()][mat.size()]; 
-    memset(visited, false, sizeof visited); 
+	if ( matrix[src.first][src.second] == 't' || matrix[dest.first][dest.second] != 'f') 
+        return -1; 
+    
+    int row = matrix.size(), col = matrix[0].size();
+    vector<vector<bool>> visited(row, vector<bool>(col,false));
     visited[src.first][src.second] = true; 
-    queue<queueNode> q; 
-    queueNode s = {src, 0}; 
-    q.push(s); 
-    while (!q.empty()) 
-    { 
-        queueNode curr = q.front(); 
-        pair<int,int> curr_pt = curr.pt; 
+    queue<Node> q; 
+    Node src_node = Node(src,0);
+    q.push(src_node); 
+    
+    while (!q.empty()){ 
+        Node curr = q.front(); 
+        pair<int,int> curr_pt = curr.cord; 
         if (curr_pt==dest) 
             return curr.dist; 
-        q.pop(); 
+        q.pop();
         for (int i = 0; i < 4; i++) 
         { 
-            int row = curr_pt.first + rowNum[i]; 
-            int col = curr_pt.second + colNum[i]; 
-            if (isValid(row, col,mat.size(),mat[0].size()) && mat[row][col]=='f' &&  
-               !visited[row][col]) 
+            int nxt_row = curr_pt.first + dir[0][i]; 
+            int nxt_col = curr_pt.second + dir[1][i]; 
+            if( isValid(nxt_row, nxt_col, row, col) && matrix[nxt_row][nxt_col] == 'f' && !visited[nxt_row][nxt_col] ) 
             { 
-                visited[row][col] = true; 
-                queueNode Adjcell = { {row, col}, 
-                                      curr.dist + 1 }; 
+                visited[nxt_row][nxt_col] = true; 
+                Node Adjcell = Node({nxt_row, nxt_col},curr.dist + 1); 
                 q.push(Adjcell); 
             } 
         } 
@@ -77,9 +80,9 @@ int main()
     pair<int,int> source = {3, 0}; 
     pair<int,int> dest = {0, 0}; 
   
-    int dist = BFS(matrix, source, dest); 
+    int dist = minSteps(matrix, source, dest); 
   
-    if (dist != INT_MAX) 
+    if (dist != -1) 
         cout << "Shortest Path is " << dist ; 
     else
         cout << "Shortest Path doesn't exist"; 
