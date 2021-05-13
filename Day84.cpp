8120cc7f83d48421bc6e5117d962ce6eceaf86a7
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2021-05-13 14:11:22 +0530

    Update Day84.cpp

--- Day84.cpp
+++ Day84.cpp
@@ -1,6 +1,7 @@
-/**************************************************************************************************************************************
+/************************************************************************************************************************************************************
 This problem was asked by Amazon.
-Given a matrix of 1s and 0s, return the number of "islands" in the matrix. A 1 represents land and 0 represents water, so an island is a group of 1s that are neighboring whose perimeter is surrounded by water.
+Given a matrix of 1s and 0s, return the number of "islands" in the matrix. A 1 represents land and 0 represents water, so an island is a group of 1s 
+that are neighboring whose perimeter is surrounded by water.
 For example, this matrix has 4 islands.
 1 0 0 0 0
 0 0 1 1 0
@@ -8,7 +9,8 @@ For example, this matrix has 4 islands.
 0 0 0 0 0
 1 1 0 0 1
 1 1 0 0 1
-***************************************************************************************************************************************/
+*************************************************************************************************************************************************************/
+
 #include <iostream>
 #include <vector>
 using namespace std;
