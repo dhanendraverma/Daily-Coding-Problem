fa65e224ffe864ac871b6d53aab71d237b1c4c3b
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2021-05-15 22:57:01 +0530

    Problem 423 Transitive Closure of Graph

--- Day423.cpp
+++ Day423.cpp
@@ -1,24 +1,6 @@
-/************************************************************************************************************************************************************
-This problem was asked by Microsoft.
-The transitive closure of a graph is a measure of which vertices are reachable from other vertices. It can be represented as a matrix M, where M[i][j] == 1 
-if there is a path between vertices i and j, and otherwise 0.
-For example, suppose we are given the following graph in adjacency list form:
-graph = [
-    [0, 1, 3],
-    [1, 2],
-    [2],
-    [3]
-]
-The transitive closure of this graph would be:
-[1, 1, 1, 1]
-[0, 1, 1, 0]
-[0, 0, 1, 0]
-[0, 0, 0, 1]
-Given a graph, find its transitive closure.
-************************************************************************************************************************************************************/
-
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 vector<vector<int>> transitiveClosure(vector<vector<int>>& adj_list){
@@ -39,10 +21,35 @@ vector<vector<int>> transitiveClosure(vector<vector<int>>& adj_list){
 }
 
 
+void DFS(vector<vector<int>>& adjList, int root, int descendant, vector<vector<int>>& closure){
+	for(auto child:adjList[descendant]){
+		if(!closure[root][child]){
+			closure[root][child] = true;
+			DFS(adjList, root, child, closure);
+		}
+	}
+}
+
+vector<vector<int>> transitiveClosureDFS(vector<vector<int>>& adj_list){
+	int V = adj_list.size();
+	vector<vector<int>> closure(V, vector<int>(V,false));
+	for(int i=0; i<V; i++){
+		// closure[i][i] = 1;
+		DFS(adj_list, i, i, closure);
+	}
+	return closure;
+}
 
 int main() {
 	vector<vector<int>> adj_list = {{0, 1, 3}, {1, 2}, {2}, {3}};
-	vector<vector<int>> transitive_closure = transitiveClosure(adj_list);
+	vector<vector<int>> transitive_closure = transitiveClosureDFS(adj_list);
+	for(auto row:transitive_closure){
+		for(auto ele:row)
+			cout<<ele<<" ";	
+		cout<<endl;
+	}
+	cout<<"------------------------"<<endl;
+	transitive_closure = transitiveClosureDFS(adj_list);
 	for(auto row:transitive_closure){
 		for(auto ele:row)
 			cout<<ele<<" ";	
